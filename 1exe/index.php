0c26c487d6b5fff93eed8dbace03dae85b17d5ff
ajvan25 <ajvankumar25@gmail.com>
2026-03-25 14:49:16 +0100

    created2file

---
+++ 1exe/index.php
@@ -0,0 +1,61 @@
+<?php
+//include "db_connect.php";
+require_once "db_connect.php";
+//echo "Retrieve the products";
+$sql = "SELECT * FROM `books`";
+//echo $sql;
+$result = mysqli_query($conn, $sql);
+//var_dump($result);
+$layout = '';
+if (mysqli_num_rows($result) > 0) {
+    //fetch data
+    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    foreach ($rows as $row) {
+
+        $layout .= "
+        <div>
+        <div class='card' my-3'>
+  <img src='{$row['picture']}' class='card-img-top' alt='{$row['title']}'>
+  <div class='card-body'>
+  
+    <h5 class='card-title'>{$row['title']}</h5>
+    <p class='card-text'>{$row['author']}</p>
+    <p class='card-text'>{$row['year']}</p>
+    <p class='card-text'>{$row['genre']}</p>
+    <a href='#' class='btn btn-primary'>Details</a>
+  </div>
+</div>
+
+
+        </div>
+        
+        ";
+    }
+    //$layout = 'We will fetch the datas in here';
+} else {
+    $layout = "<h3> No data found </h3>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class=" row-row cols-1 row-cols-md-2 row-cols-lg-3">
+            <?= $layout ?>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+
+
+</html>
